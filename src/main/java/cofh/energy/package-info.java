b7aa4d30118b8e8979d608f3ec4cebfe668693d3
Christian Teichmann <ckt082207@gmail.com>
2024-04-25 18:21:23 -0400

    RF implementation

---
+++ src/main/java/cofh/energy/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
+ * http://www.teamcofh.com
+ */
+@API(apiVersion = CoFHAPIProps.VERSION, owner = "cofhapi", provides = "cofhapi|energy")
+package cofh.energy;
+
+import cofh.CoFHAPIProps;
+import cpw.mods.fml.common.API;
