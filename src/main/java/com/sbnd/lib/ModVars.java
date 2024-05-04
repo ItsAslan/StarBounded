e20155590da00ee688bdb4bf4ecc960ed44220ba
Christian Teichmann <ckt082207@gmail.com>
2024-05-04 10:30:45 -0400

    Release 0.0.4

--- src/main/java/com/sbnd/lib/ModVars.java
+++ src/main/java/com/sbnd/lib/ModVars.java
@@ -7,7 +7,7 @@ public class ModVars
     public static final String MOD_NAME = "StarBounded";
 
     // Mod Version
-    public static final String MOD_VERSION = "0.0.3";
+    public static final String MOD_VERSION = "0.0.4";
 
     /* NAMING CONVENTION
 
